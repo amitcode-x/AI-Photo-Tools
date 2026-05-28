a2ff14ae32b28f467399c48ed2588908e809cd6b
amitcode-x <amitchauhan6599@gmail.com>
2026-05-28 18:23:03 +0530

    fix render media storage

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,19 +1,19 @@
-import axios from "axios"
-
-const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
-})
-
-export default API
-
-
-
-
-
-// import axios from "axios";
+// import axios from "axios"
 
 // const API = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
+//   baseURL: "http://127.0.0.1:8000/api/",
+// })
 
-// export default API;
+// export default API
+
+
+
+
+
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export default API;
